a0c670f6a1f466fcbf0dc9a4e85e3cf461aa4db7
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2021-07-05 16:26:38 +0700

    Increase number of cores used to build cubeslam

--- install_dependenices.sh
+++ install_dependenices.sh
@@ -4,19 +4,19 @@ cd object_slam/Thirdparty/g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
+make -j8
 cd ../../../..
 
 cd orb_object_slam/Thirdparty/g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
+make -j8
 cd ../../../..
 
 cd orb_object_slam/Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j2
+make -j8
 cd ../../../..
